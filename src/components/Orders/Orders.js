9ed6f178906ba6f44f5dc14df4bff56bfaeb4a1b
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-22 10:52:35 -0600

    Finish delete functionality on delete click

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import './Orders.css';
-import { connect } from 'react-redux';
-import { setOrders } from '../../actions';
-import { getOrders } from '../../apiCalls';
+import React, { Component } from "react";
+import "./Orders.css";
+import { connect } from "react-redux";
+import { setOrders } from "../../actions";
+import { getOrders, sendDeleteRequest } from "../../apiCalls";
 
 class Orders extends Component {
   componentDidMount() {
     getOrders()
       .then(data => this.props.setOrders(data.orders))
-      .catch(err => console.error('Error fetching:', err));
+      .catch(err => console.error("Error fetching:", err));
   }
 
-  deleteOrder() {
-    
+  deleteOrder(id) {
+    sendDeleteRequest(id).catch(err => console.error("Error deleting: ", err));
+    const filteredOrders = this.props.orders.filter(order => order.id !== id);
+    this.props.setOrders(filteredOrders);
   }
 
   render() {
@@ -23,19 +25,21 @@ class Orders extends Component {
           <h3>{order.name}</h3>
           <ul className="ingredient-list">
             {order.ingredients.map((ingredient, index) => {
-              return <li key={index} data-testid={`${ingredient}-${index}`}>{ingredient}</li>
+              return (
+                <li key={index} data-testid={`${ingredient}-${index}`}>
+                  {ingredient}
+                </li>
+              );
             })}
           </ul>
-          <button onClick={this.deleteOrder(order.id)}>delete</button>
+          <button onClick={() => this.deleteOrder(order.id)}>delete</button>
         </div>
-      )
+      );
     });
 
     return (
-      <section>
-        { orderEls.length ? orderEls : <p>No orders yet!</p> }
-      </section>
-    )
+      <section>{orderEls.length ? orderEls : <p>No orders yet!</p>}</section>
+    );
   }
 }
 
@@ -43,10 +47,8 @@ const mapStateToProps = ({ orders }) => ({
   orders
 });
 
-const mapDispatchToProps = dispatch => (
-  {
-    setOrders: (orders) => dispatch(setOrders(orders))
-  }
-);
+const mapDispatchToProps = dispatch => ({
+  setOrders: orders => dispatch(setOrders(orders))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Orders);
