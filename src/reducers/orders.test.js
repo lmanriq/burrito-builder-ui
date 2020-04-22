15e9df6cd023a3fd691f81674ea6c52f854b43fe
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-22 10:08:36 -0600

    Add set orders and add orders tests

--- src/reducers/orders.test.js
+++ src/reducers/orders.test.js
@@ -1,9 +1,53 @@
-import { orders } from './orders';
+import { orders } from "./orders";
 
-describe('loginFlow', () => {
-  it('should return the initial state', () => {
+describe("loginFlow", () => {
+  it("should return the initial state", () => {
     const expectedResult = [];
-    const result = orders(undefined, ['order']);
-    expect(result).toEqual(expectedResult)
-  })
+    const result = orders(undefined, ["order"]);
+    expect(result).toEqual(expectedResult);
+  });
+  it("when receiving SET_ORDERS action, it should return the orders array", () => {
+    const sampleOrders = [
+      {
+        id: 1,
+        name: "Pat",
+        ingredients: [
+          "beans",
+          "lettuce",
+          "carnitas",
+          "queso fresco",
+          "jalapeno"
+        ]
+      },
+      {
+        id: 2,
+        name: "Sue",
+        ingredients: ["carnitas", "jalapeno"]
+      }
+    ];
+    const sampleAction = {
+      type: "SET_ORDERS",
+      orders: sampleOrders
+    };
+    const result = orders([], sampleAction);
+    expect(result).toEqual(sampleOrders);
+  });
+  it("when receiving ADD_ORDER action, it should return the updated orders array", () => {
+    const order1 = {
+      id: 1,
+      name: "Pat",
+      ingredients: ["beans", "lettuce", "carnitas", "queso fresco", "jalapeno"]
+    };
+    const order2 = {
+      id: 2,
+      name: "Sue",
+      ingredients: ["carnitas", "jalapeno"]
+    };
+    const sampleAction = {
+      type: "ADD_ORDER",
+      order: order2
+    };
+    const result = orders([order1], sampleAction);
+    expect(result).toEqual([order1, order2]);
+  });
 });
