ab65c72ca09a531f8226a731bb1abe7f5bb03d1e
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-22 09:21:43 -0600

    Make App functional and move component did mount method

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,33 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
-import { getOrders } from '../../apiCalls';
-import Orders from '../../components/Orders/Orders';
-import OrderForm from '../../components/OrderForm/OrderForm';
+import React from "react";
+import "./App.css";
+import Orders from "../../components/Orders/Orders";
+import OrderForm from "../../components/OrderForm/OrderForm";
 
-class App extends Component {
-  constructor(props) {
-    super();
-    this.props = props;
-  }
-
-  componentDidMount() {
-    getOrders()
-      .then(data => this.props.setOrders(data.orders))
-      .catch(err => console.error('Error fetching:', err));
-  }
-
-  render() {
-    return (
-      <main className="App">
-        <header>
-          <h1>Burrito Builder</h1>
-          <OrderForm />
-        </header>
-
-        <Orders orders={this.props.orders}/>
-      </main>
-    );
-  }
-}
+const App = () => {
+  return (
+    <main className="App">
+      <header>
+        <h1>Burrito Builder</h1>
+        <OrderForm />
+      </header>
+      <Orders />
+    </main>
+  );
+};
 
 export default App;
